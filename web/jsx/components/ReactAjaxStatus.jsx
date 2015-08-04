5b7ff134a77093b0ad7cb471e2cb9cd336ce13dd
Lionel Cocatrix <yoyolebatteur@gmail.com>
2015-08-04 16:12:58 +0200

    Ajax Status component

---
+++ web/jsx/components/ReactAjaxStatus.jsx
@@ -0,0 +1,26 @@
+'use strict';
+
+var React = require('react/addons');
+var Reflux = require('reflux');
+var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
+
+var InstanceFormStore = require('../stores/InstanceFormStore.jsx');
+
+var ReactAjaxStatus = React.createClass({
+    mixins: [Reflux.connectFilter(InstanceFormStore, "status", function(instance) {
+            return instance.status;
+        }
+    )],
+    render: function () {
+        var okIcon = this.state.status.success[this.props.status] === true ? <span key="ajax-ok" className="glyphicon glyphicon-ok" aria-hidden="true"></span> : null;
+        return (
+            <div className="ajax-success">
+                    <ReactCSSTransitionGroup transitionName="ajax-success">
+                        { okIcon }
+                    </ReactCSSTransitionGroup>
+                </div>
+        );
+    }
+});
+
+module.exports = ReactAjaxStatus;
