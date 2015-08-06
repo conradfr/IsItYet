7e56c1a002999a5991ba845895c2c15fb0ffe782
Lionel Cocatrix <yoyolebatteur@gmail.com>
2015-08-06 23:31:22 +0200

    Update websocket server port

--- websocket/server.php
+++ websocket/server.php
@@ -12,7 +12,9 @@ $client->connect(array($pusher, 'init'));
 
 // Set up our WebSocket server for clients wanting real-time updates
 $webSock = new React\Socket\Server($loop);
-$webSock->listen(8080, '0.0.0.0'); // Binding to 0.0.0.0 means remotes can connect
+
+/* @todo use app config file to get the port */
+$webSock->listen(1080, '0.0.0.0'); // Binding to 0.0.0.0 means remotes can connect
 $webServer = new Ratchet\Server\IoServer(
     new Ratchet\Http\HttpServer(
         new Ratchet\WebSocket\WsServer(
